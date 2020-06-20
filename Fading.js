8e63a56db78c5ee4020fba3cb5e325c3ea9bbedb
Blewkse <julesbloux@gmail.com>
2020-06-20 21:25:55 +0200

    Pose des videos animations

--- Fading.js
+++ Fading.js
@@ -1,13 +1,35 @@
 
 $(document).ready(function(){
+    
     $(window).scroll(function(){
-        var percent = $(document).scrollTop()  / 200;
+        console.log($(document).scrollTop());
+        var percent = $(document).scrollTop()  / 400;
         
         if ($(this).scrollTop() > 20){
-            $(".fond1").css("opacity",1 - percent); 
+            $(".fond1").css("opacity",1 - percent);
+            
         }
           else {
             $(".fond1").css({"opacity" : "1"});
+        }
+         if ($(this).scrollTop() > 390){
+            $(".fond1").css("z-index","0");
+            
+        }
+          else {
+            $(".fond1").css({"z-index" : "50"});
+        }
+    })
+})
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var percent = $(document).scrollTop()  / 400;
+        
+        if ($(this).scrollTop() > 100){
+            $(".titrekenzo").css("opacity",1 - percent); 
+        }
+          else {
+            $(".titrekenzo").css({"opacity" : "1"});
         }
     })
 })
